2134d060ce441c15a474671d4dbd2d00f0a5ae3c
fumipi <toyodafumiko@gmail.com>
2024-03-26 13:44:55 +0900

    first commit

---
+++ controllers/penbot/penbot.py
@@ -0,0 +1,118 @@
+from controller import Supervisor
+import math
+from svgpathtools import svg2paths2
+from math import ceil
+import numpy as np
+
+class Controller(Supervisor):
+    timeStep = 64
+    step_size = 0.01
+
+    def __init__(self):
+        super(Controller, self).__init__()
+        self.robot = self.getFromDef('MyBot')
+        self.trans_field = self.robot.getField('translation')
+        self.rotation_field = self.robot.getField('rotation')
+        self.pen = self.getDevice('pen')
+        self.pen.write(True)
+        self.current_position = [0, 0]
+        self.current_rotation = 0
+        self.pen_is_down = True
+
+    def forward(self, distance):
+        dx = distance * math.cos(self.current_rotation)
+        dy = distance * math.sin(self.current_rotation)
+        new_position = [self.current_position[0] + dx, self.current_position[1] + dy]
+        self.move_to(new_position)
+
+    def backward(self, distance):
+        self.forward(-distance)
+
+    def left(self, angle):
+        self.current_rotation += math.radians(angle)
+
+    def right(self, angle):
+        self.left(-angle)
+
+    def goto(self, x, y):
+        new_position = [x, y]
+        self.move_to(new_position)
+
+    def move_to(self, position):
+        steps = int(math.dist(self.current_position, position) / self.step_size)
+        for i in range(steps):
+            x = self.current_position[0] + (position[0] - self.current_position[0]) * i / steps
+            y = self.current_position[1] + (position[1] - self.current_position[1]) * i / steps
+            self.trans_field.setSFVec3f([x, y, 0])
+            self.step(self.timeStep)
+        self.current_position = position
+
+    def penup(self):
+        self.pen.write(False)
+        self.pen_is_down = False
+
+    def pendown(self):
+        self.pen.write(True)
+        self.pen_is_down = True
+
+    def move(self, x, y):
+        self.penup()
+        self.goto(x, y)
+        self.pendown()
+
+    def line(self, x, y):
+        self.goto(x, y)
+
+    def draw_polygon(self, poly):
+        self.penup()
+        self.goto(poly[0][0], poly[0][1])
+        self.pendown()
+        for p in poly[1:]:
+            self.goto(p[0], p[1])
+
+    def draw_multipolygon(self, mpoly):
+        for poly in mpoly:
+            self.draw_polygon(poly)
+
+    def run(self):
+        svg_file = "../../inputs/svg_icon.svg"
+        paths, attrs, svg_attr = svg2paths2(svg_file)
+        
+        viewbox = svg_attr.get('viewBox', '0 0 100 100')
+        viewbox_values = viewbox.split()
+        svg_size = float(viewbox_values[2]), float(viewbox_values[3])
+        
+        # 紙のサイズを取得（将来的にはセンサーで取得する）
+        root_node = self.getRoot()
+        paper_node = root_node.getField('children').getMFNode(-1)
+        paper_size = paper_node.getField('children').getMFNode(0).getField('geometry').getSFNode().getField('size').getSFVec2f()
+        
+        # 紙のサイズにSVGを拡大縮小する
+        scale_x = paper_size[0] / svg_size[0]
+        scale_y = paper_size[1] / svg_size[1]
+        scale_factor = min(scale_x, scale_y)
+        
+        # SVGの座標からWebotの座標に変換
+        offset_x = -svg_size[0] * scale_factor / 2
+        offset_y = svg_size[1] * scale_factor / 2  
+        
+        seg_res = 5
+        polys = []
+        for path, attr in zip(paths, attrs):
+            if 'stroke' in attr and attr['stroke'] != 'none':
+                poly = []
+                for subpaths in path.continuous_subpaths():
+                    points = []
+                    for seg in subpaths:
+                        interp_num = ceil(seg.length() / seg_res)
+                        points.append(seg.point(np.arange(interp_num) / interp_num))
+                    points = np.concatenate(points)
+                    points = np.append(points, points[0])
+                    poly.append(points)
+                polys.append([[(p.real * scale_factor + offset_x, -p.imag * scale_factor + offset_y) for p in pl] for pl in poly])  # Flipped y-coordinate
+        
+        for poly in polys[1:]:  # 枠線はスキップ
+            self.draw_multipolygon(poly) 
+
+controller = Controller()
+controller.run()
